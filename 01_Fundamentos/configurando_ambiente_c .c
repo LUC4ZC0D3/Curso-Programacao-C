6ad47eaf9907bd7d5b0a28464c5b5c845717e898
Lucas Lima da Silva Mello <lucaslsmello1234@gmail.com>
2026-02-25 23:46:45 -0300

    Configuração do .gitignore e primeiro codigo C com a mensagem "Olá Mundo!"

---
+++ 01_Fundamentos/configurando_ambiente_c .c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    printf("Olá Mundo!\n");
+    printf("Meu ambiente C configurado e rodando!\n");
+
+    return 0;
+}
